58060e4ab3c99f16d238db4c653326407fbc1ce2
John Ferguson Smart <john.smart@wakaleo.com>
2022-08-01 12:25:24 +0100

    Sample solution

--- src/test/java/com/serenitydojo/WhenCreatingObjects.java
+++ src/test/java/com/serenitydojo/WhenCreatingObjects.java
@@ -40,13 +40,13 @@ public class WhenCreatingObjects {
     @Test
     public void whenAnimalsPlay() {
 // TODO: Uncomment me
-//
-//        Pet fido = new Dog("Fido","Bone", 5);
-//        Pet spot = new Cat("Spot","String", 5);
-//        Pet hazel = new Hamster("Hazel", 1, "Wheel");
-//
-//        assertThat(fido.play(), equalTo("plays with bone"));
-//        assertThat(spot.play(), equalTo("plays with string"));
-//        assertThat(hazel.play(), equalTo("runs in wheel"));
+
+        Pet fido = new Dog("Fido","Bone", 5);
+        Pet spot = new Cat("Spot","String", 5);
+        Pet hazel = new Hamster("Hazel", 1, "Wheel");
+
+        assertThat(fido.play(), equalTo("plays with bone"));
+        assertThat(spot.play(), equalTo("plays with string"));
+        assertThat(hazel.play(), equalTo("runs in wheel"));
     }
 }
